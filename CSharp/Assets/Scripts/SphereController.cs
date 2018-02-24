c9dc2017eb8ffdb32cc64310df7b58d7a3696923
Charlie Rode <charlie.rode@ihsmarkit.com>
2018-02-23 16:19:28 -0800

    first commit

---
+++ CSharp/Assets/Scripts/SphereController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour {
+    public float Control;
+    public float OtherControl;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 v = new Vector3();
+        v.x = Mathf.Sin(Control) * OtherControl;
+        v.y = Mathf.Cos(Control) * OtherControl;
+        v.z = Control * OtherControl;
+
+        transform.position = v;
+	}
+}
